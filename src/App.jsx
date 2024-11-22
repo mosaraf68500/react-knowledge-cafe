4068ba456508df8d242a1e47a90ab5eb6ee090ba
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2024-11-22 10:30:32 +0600

    index

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <h1 className=' text-5xl bg-slate-100'>React knowledge cafe</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
